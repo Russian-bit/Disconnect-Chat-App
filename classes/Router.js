e26d3a2a7b1789ef4b574c9588fcbf039badaad3
Nicole Tretyakova <nicolet31941@gmail.com>
2020-08-19 16:43:39 +1200

    Profile page views update 0.1

--- classes/Router.js
+++ classes/Router.js
@@ -1,4 +1,5 @@
 var UserController = require('../controllers/UserController');
+var ChannelController = require('../controllers/ChannelController');
 var createError = require('http-errors');
 
 /**
@@ -57,6 +58,7 @@ class Router{
      */
     addControllers() {
         var userController = new UserController();
+        var channelController = new ChannelController();
     }
 
     // Renders home page ./views/index.ejs
